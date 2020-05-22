ed67635d94e9d6f694b9245e22168fae0be636dc
tfwcn@sina.cn <tfwcn@sina.cn>
2020-05-22 22:14:42 +0800

    init

--- README.md
+++ README.md
@@ -1,2 +1,74 @@
-# tensorflow2-disout
+
 tensorflow2实现的disout
+
+```
+python train.py
+```
+
+# Disout: Feature Map Distortion to Regularize Deep Neural Networks
+
+This code is the Pytorch implementation of AAAI 2020 paper [Beyond Dropout: Feature Map Distortion to Regularize Deep Neural Networks](https://arxiv.org/abs/2002.11022).
+
+Deep neural networks often consist of a great number of trainable parameters for extracting powerful features from given datasets. On one hand, massive trainable parameters significantly enhance the performance of these deep networks. On the other hand, they bring the problem of over-fitting. To this end, dropout based methods disable some elements in the output feature maps during the training phase for reducing the co-adaptation of neurons. Although the generalization ability of the resulting models can be enhanced by these approaches, the conventional binary dropout is not the optimal solution. Therefore, we investigate the empirical Rademacher complexity related to intermediate layers of deep neural networks and propose a feature distortion method (Disout) for addressing the aforementioned problem. In the training period, randomly selected elements in the feature maps will be replaced with specific values by exploiting the generalization error bound. The superiority of the proposed feature map distortion for producing deep neural network with higher testing performance is analyzed and demonstrated on several benchmark image datasets.
+
+<p align="center">
+<img src="images/disout.png" width="800">
+</p>
+
+
+## Requirements
+
+- python 3
+- pytorch >= 1.0.0
+- torchvision
+
+## Usage
+
+Disout is a flexible block that can insert into deep neural networks and improve their generalization ability. To achieve the optimal performance, the following three hyper-parameters should be adjusted.
+
+```
+dist_prob: probability of an element to be distorted.
+
+block_size: size of the block to be distorted.
+
+alpha: the intensity of distortion.
+```
+
+Here we provide a simple example to illustrate how to implement disout in neural networks. For example, you can run the following code to train a ResNet-56 on CIFAR-10 dataset.
+
+```python
+python train.py --dist_prob 0.09 --alpha 5.0 --block_size 6
+```
+
+For ResNet-50 on ImageNet, you can run
+
+```python
+python train_imagenet.py --dist_prob 0.07 --alpha 1.0 --block_size 7
+```
+
+## Results
+
+<p align="center">
+<img src="images/result.png" width="800">
+</p>
+
+<p align="center">
+<img src="images/result2.png" width="800">
+</p>
+
+You should achieve about 94.50% accuracy on CIFAR-10, and 78.76% top-1 accuracy on ImageNet with the default setting.
+
+## Citation
+
+	@article{tang2020beyond,
+	  title={Beyond Dropout: Feature Map Distortion to Regularize Deep Neural Networks},
+	  author={Tang, Yehui and Wang, Yunhe and Xu, Yixing and Shi, Boxin and Xu, Chao and Xu, Chunjing and Xu, Chang},
+	  journal={arXiv preprint arXiv:2002.11022},
+	  year={2020}
+	}
+
+## Contributing
+
+We appreciate all contributions. If you are planning to contribute back bug-fixes, please do so without any further discussion.
+
+If you plan to contribute new features, utility functions or extensions to the core, please first open an issue and discuss the feature with us. Sending a PR without discussion might end up resulting in a rejected PR, because we might be taking the core in a different direction than you might be aware of.
---
+++ train.py
@@ -0,0 +1,104 @@
+import tensorflow as tf
+from disout_tf2 import Disout
+
+
+class CustomLayer(tf.keras.layers.Layer):
+    '''自定义层'''
+
+    def __init__(self, units, activation, **args):
+        super(CustomLayer, self).__init__(**args)
+        self.units = units
+        self.activation = activation
+
+    def build(self, input_shape):
+        '''初始化网络'''
+        num_inputs = input_shape[-1]
+        self.kernel = self.add_weight('kernel',
+                                      shape=[num_inputs,
+                                             self.units],
+                                      initializer=tf.keras.initializers.he_normal()
+                                      )
+        self.bias = self.add_weight("bias",
+                                    shape=[self.units],
+                                    initializer=tf.keras.initializers.Zeros
+                                    )
+        self.activation_layer = tf.keras.layers.Activation(self.activation)
+
+    def call(self, x):
+        '''运算部分'''
+        print('call动态图会不断打印该信息，静态图只打印数次。', type(x), x.shape)
+        output = tf.matmul(x, self.kernel) + self.bias
+        output = self.activation_layer(output)
+        return output
+
+    def compute_output_shape(self, input_shape):
+        '''计算输出shape'''
+        return (input_shape[0], self.output_dim)
+
+
+class CustomModel(tf.keras.Model):
+    '''自定义模型'''
+
+    def __init__(self, conv_size, hidden_size, dropout_rate):
+        '''初始化模型层'''
+        super().__init__()
+        self.conv1 = tf.keras.layers.Conv2D(
+            filters=32, kernel_size=conv_size, activation='relu')
+        self.disout1 = Disout(0.09, block_size=5)
+        self.pool1 = tf.keras.layers.MaxPool2D(pool_size=2)
+        self.conv2 = tf.keras.layers.Conv2D(
+            filters=64, kernel_size=conv_size, activation='relu')
+        self.disout2 = Disout(0.09, block_size=3)
+        self.pool2 = tf.keras.layers.MaxPool2D(pool_size=2)
+        self.flatten = tf.keras.layers.Flatten()
+        self.fc1 = CustomLayer(units=hidden_size, activation='relu')
+        self.dropout = tf.keras.layers.Dropout(rate=dropout_rate)
+        self.fc2 = CustomLayer(units=10, activation='softmax')
+
+    def call(self, x):
+        '''运算部分'''
+        x = self.conv1(x)
+        x = self.disout1(x)
+        x = self.pool1(x)
+
+        x = self.conv2(x)
+        x = self.disout2(x)
+        x = self.pool2(x)
+        x = self.flatten(x)
+        x = self.fc1(x)
+        x = self.dropout(x)
+        return self.fc2(x)
+
+
+def main():
+    mnist = tf.keras.datasets.mnist
+
+    (x_train, y_train), (x_test, y_test) = mnist.load_data()
+    x_train, x_test = x_train / 255.0, x_test / 255.0
+    x_train, x_test = tf.expand_dims(
+        x_train, axis=-1), tf.expand_dims(x_test, axis=-1)
+    y_train, y_test = tf.one_hot(y_train, 10), tf.one_hot(y_test, 10)
+    print('x_train, y_train', x_train.shape, y_train.shape)
+    print('x_test, y_test', x_test.shape, y_test.shape)
+
+    # 卷积实现图片分类
+    model = CustomModel(5, 128, 0.65)
+
+    model.compile(
+        optimizer=tf.keras.optimizers.Adam(),
+        loss=tf.keras.losses.CategoricalCrossentropy(),
+        metrics=['accuracy'])
+
+    model.fit(x_train, y_train, epochs=10, validation_data=(x_test, y_test))
+
+
+if __name__ == '__main__':
+    # 设置GPU显存自适应
+    gpus = tf.config.experimental.list_physical_devices(device_type='GPU')
+    cpus = tf.config.experimental.list_physical_devices(device_type='CPU')
+    print(gpus, cpus)
+    for gpu in gpus:
+        tf.config.experimental.set_memory_growth(gpu, True)
+    # tf.config.experimental.set_visible_devices(gpus[0], 'GPU')
+    tf.config.experimental.set_visible_devices(cpus[0], 'CPU')
+    main()
